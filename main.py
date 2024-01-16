8589f1e906b578a4d5eef9e1c9e0936b4f7afdcc
dnszhkv <den1szhuk0v@yandex.ru>
2024-01-16 22:53:37 +0700

    summary
    Реализация метода submitData v1

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import psycopg2
-from config import *
+from flask import Flask, request, jsonify
+from decouple import config
+import json
 
+# Получаем данные для подключения к базе данных из переменных окружения
+host = config('FSTR_DB_HOST')
+port = config('FSTR_DB_PORT')
+user = config('FSTR_DB_LOGIN')
+password = config('FSTR_DB_PASS')
+dbname = config('FSTR_DB_NAME')
 
+# Создание базы данных
 try:
     # создаю подключение к базе данных
     conn = psycopg2.connect(
         host=host,
+        port=port,
         user=user,
         password=password,
         database=dbname,
@@ -98,3 +108,164 @@ finally:
     if conn:
         conn.close()
         print('[INFO] PostgresSQL connection is closed')
+
+
+# Метод SubmitData
+app = Flask(__name__)
+
+
+class PerevalDatabase:
+    def __init__(self):
+        try:
+            # создаю подключение к базе данных
+            self.conn = psycopg2.connect(
+                host=host,
+                port=port,
+                user=user,
+                password=password,
+                database=dbname,
+            )
+
+            self.conn.autocommit = True
+
+        except Exception as ex:
+            print('[INFO] Error while working with PostgreSQL', ex)
+
+    def submit_data(self, data):
+        try:
+            # создаю курсор для выполнения запросов
+            with self.conn.cursor() as cursor:
+                # добавление пользователя
+                user = data.get("user", {})
+                email = user.get("email")
+                phone = user.get("phone")
+                fam = user.get("fam")
+                name = user.get("name")
+                otc = user.get("otc")
+                cursor.execute(
+                    """
+                    INSERT INTO "public"."Users" ("email", "phone", "fam", "name", "otc")
+                    VALUES (%s, %s, %s, %s, %s)
+                    ON CONFLICT ("email") DO NOTHING;
+                    """,
+                    (email, phone, fam, name, otc),
+                )
+
+                # добавление координат
+                coords = data.get("coords", {})
+                latitude = coords.get("latitude")
+                longitude = coords.get("longitude")
+                height = coords.get("height")
+                cursor.execute(
+                    """
+                    INSERT INTO "public"."Coords" ("latitude", "longitude", "height")
+                    VALUES (%s, %s, %s)
+                    RETURNING "id";
+                    """,
+                    (latitude, longitude, height),
+                )
+                coord_id = cursor.fetchone()[0]
+
+                # добавление уровня сложности
+                level = data.get("level", {})
+                winter = level.get("winter")
+                summer = level.get("summer")
+                autumn = level.get("autumn")
+                spring = level.get("spring")
+
+                # добавление изображений
+                images = data.get("images", [])
+                image_ids = []
+                for img_data in images:
+                    cursor.execute(
+                        """
+                        INSERT INTO "public"."PerevalImages" ("date_added", "img")
+                        VALUES (CURRENT_TIMESTAMP, %s)
+                        RETURNING "id";
+                        """,
+                        (img_data["data"],),
+                    )
+                    image_id = cursor.fetchone()[0]
+                    image_ids.append(image_id)
+
+                # добавление перевала
+                cursor.execute(
+                    """
+                    INSERT INTO "public"."pereval_added" (
+                        "date_added", "beautyTitle", "title", "other_titles", "connect", "add_time",
+                        "raw_data", "coord_id", "level_winter", "level_summer", "level_autumn", "level_spring", "user_id"
+                    )
+                    VALUES (
+                        CURRENT_TIMESTAMP, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,
+                        (SELECT "id" FROM "public"."Users" WHERE "email" = %s)
+                    )
+                    RETURNING "id";
+                    """,
+                    (
+                        data.get("beauty_title"),
+                        data.get("title"),
+                        data.get("other_titles"),
+                        data.get("connect"),
+                        data.get("add_time"),
+                        data.get("raw_data"),
+                        coord_id,
+                        winter,
+                        summer,
+                        autumn,
+                        spring,
+                        email,
+                    ),
+                )
+                pereval_id = cursor.fetchone()[0]
+
+                # связывание изображений с перевалом
+                for image_id in image_ids:
+                    cursor.execute(
+                        """
+                        INSERT INTO "public"."PerevalAddedImages" ("pereval_added_id", "image_id")
+                        VALUES (%s, %s);
+                        """,
+                        (pereval_id, image_id),
+                    )
+
+            return {"status": 200, "message": "Отправлено успешно", "id": pereval_id}
+
+        except Exception as ex:
+            print('[INFO] Error while submitting data to PostgreSQL', ex)
+            return {"status": 500, "message": "Ошибка при выполнении операции", "id": None}
+
+    def close_connection(self):
+        if self.conn:
+            self.conn.close()
+            print('[INFO] PostgreSQL connection is closed')
+
+
+# Создаю экземпляр класса PerevalDatabase
+pereval_db = PerevalDatabase()
+
+
+@app.route('/submitData', methods=['POST'])
+def submit_data():
+    try:
+        # Получаю данные из тела запроса
+        data = request.get_json()
+
+        # Вызываю метод submit_data для обработки данных и добавления в базу данных
+        result = pereval_db.submit_data(data)
+
+        return jsonify(result)
+
+    except Exception as ex:
+        print('[INFO] Error while processing request', ex)
+        return jsonify({"status": 500, "message": "Internal Server Error", "id": None})
+
+
+if __name__ == '__main__':
+    # Считываем данные из файла data.json
+    with open('data.json', 'r', encoding='utf-8') as file:
+        json_data = json.load(file)
+
+    # Вызываем метод submit_data для обработки данных и добавления в базу данных
+    result = pereval_db.submit_data(json_data)
+
+    app.run(debug=True)
